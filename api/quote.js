af749e0ff2f514ec420cbd3a80d52d08f81de9f7
mramirezusa-sudo <mramirezusa@gmail.com>
2026-06-19 00:41:55 +0200

    Update quote.js

--- api/quote.js
+++ api/quote.js
@@ -1,4 +1,9 @@
 export default async function handler(req, res) {
+	
+res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Methods", "POST, GET");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
   try {
     const data = req.body;
 
