fd11f05f58d14ba1af4fb90fb8d42407902fe1a1
mramirezusa-sudo <mramirezusa@gmail.com>
2026-06-19 10:59:03 +0200

    Update quote.js

--- api/quote.js
+++ api/quote.js
@@ -1,12 +1,17 @@
 export default async function handler(req, res) {
-	
-res.setHeader("Access-Control-Allow-Origin", "*");
-res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-if (req.method === "OPTIONS") {
-  return res.status(200).end();
-}
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Max-Age", "86400");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
 
   try {
     const data = req.body;
